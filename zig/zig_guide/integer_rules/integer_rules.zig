e55d8608f3af23e9a078388ab712f34061978828
Ewald Horn <ewaldhorn@gmail.com>
2024-07-29 15:02:03 +0200

    Zig refreshers

--- zig/zig_guide/integer_rules/integer_rules.zig
+++ zig/zig_guide/integer_rules/integer_rules.zig
@@ -1,1 +1,14 @@
-const std = @import("std");
+const expect = @import("std").testing.expect;
+
+test "integer widening" {
+    const a: u8 = 250;
+    const b: u16 = a;
+    const c: u32 = b;
+    try expect(c == a);
+}
+
+test "@intCast" {
+    const x: u64 = 200;
+    const y = @as(u8, @intCast(x));
+    try expect(@TypeOf(y) == u8);
+}
