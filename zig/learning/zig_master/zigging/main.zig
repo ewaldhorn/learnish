d2ba7a2a242fe5c8bcc4c592780fa41e6639bcd8
Ewald Horn <ewaldhorn@gmail.com>
2024-09-05 14:14:40 +0200

    Getting skeleton in place

--- zig/learning/zig_master/zigging/main.zig
+++ zig/learning/zig_master/zigging/main.zig
@@ -5,22 +5,37 @@ fn newline() void {
     print("\n", .{});
 }
 // ----------------------------------------------------------------------------
-fn print_evens(max: i8) void {
-    const start: i8 = 2;
+fn print_evens(max: u8) void {
+    const start: u8 = 2;
     print("Even numbers from {} to {} are:\n", .{ start, max });
 }
 
 // ----------------------------------------------------------------------------
-fn print_odds(max: i8) void {
-    const start: i8 = 2;
+fn print_odds(max: u8) void {
+    const start: u8 = 2;
     print("Odd numbers from {} to {} are:\n", .{ start, max });
+}
+
+// ----------------------------------------------------------------------------
+fn sumOf(left: u16, right: u16) u32 {
+    return left + right;
 }
 
 // ======================================================================= MAIN
 pub fn main() !void {
-    const max: i8 = 10;
+    const max: u8 = 10;
 
     print_evens(max);
     newline();
     print_odds(max);
+}
+
+// ====================================================================== TESTS
+const assert = std.debug.assert;
+
+test "sum_of 8 and 8" {
+    const left: u8 = 8;
+    const right: u8 = 8;
+    const sum = sumOf(left, right);
+    assert(sum == 16);
 }
