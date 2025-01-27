b1337c5260c47fad7cf46f8af918818e4bf0e6ee
Ewald Horn <ewaldhorn@gmail.com>
2025-01-27 13:32:17 +0200

    Cleaning up the code now that it works

--- zig/raylib/rlexample/src/main.zig
+++ zig/raylib/rlexample/src/main.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+// ----------------------------------------------------------------------------
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 600;
+const WINDOW_TITLE = "Hello Raylib from Zig!";
+
+// ----------------------------------------------------------------------------
 pub fn main() !void {
-    rl.initWindow(800, 600, "Hello Raylib from Zig!");
+    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     defer rl.closeWindow();
 
     while (!rl.windowShouldClose()) {
